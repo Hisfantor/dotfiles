9c5600a316cd6911676d9c68e0e6d6462022e38d
Hisfantor <i.nici@icloud.com>
2018-06-30 22:37:24 +0200

    should be all

---
+++ .config/i3/wp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm ~/Pictures/Wallpaper-reddit/wallpaper0.jpg
+wallpaper-reddit --random
+wallpaper-reddit --save
+feh --bg-scale /home/hisfantor/Pictures/Wallpaper-reddit/wallpaper0.jpg
+
